5d7ec72220b019a38615ed1d84c25e47f443507c
Roee Zolantz <zolantz.roee@gmail.com>
2019-04-30 01:47:22 +0300

    Added docs in ItemsList in comments

--- src/ItemsList.jsx
+++ src/ItemsList.jsx
@@ -7,7 +7,19 @@ export default function ItemsPage() {
 	const props = {
 		items: state.items
   };
-  
+
+// In this component you will have the items list that come back from the ML server, 
+// because i take them in the main window to validate the receipt (for example - if there are 0 items its an error)
+
+// This is how to take items from the state
+// state.WhatEverYouWant
+
+  // This is how to do setState using the reducers inside Store.js
+// dispatch({
+//     type: "Reducer's-Type, according to the cases in the main reducer",
+//     payload: the data to transfer into the reducer
+// });
+
   return (
       <div>
         <p>Hello world</p>
