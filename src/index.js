76173af02e0cffbec8ef3b04a6fb91a1c5e1e8c1
NaveCohen <nave.coheng@gmail.com>
2019-05-17 21:53:36 +0300

    Finish saving to firebase and fix firebase interaction

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import {
   createMuiTheme,
 } from '@material-ui/core/styles';
 import App from './App';
+import Firebase, { FirebaseContext } from './Firebase';
 
 // Configure JSS //for rtl support
 const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
@@ -24,15 +25,19 @@ const theme = createMuiTheme({
 // Custom Material-UI class name generator. //for rtl support
 const generateClassName = createGenerateClassName();
 
+const firebase = Firebase.initialize();
+
 const RootComp = props => {
   return (
-    <StoreProvider>
-      <JssProvider jss={jss} generateClassName={generateClassName}>
-        <MuiThemeProvider theme={theme}>
-          <App />
-        </MuiThemeProvider>
-      </JssProvider>
-    </StoreProvider>
+    <FirebaseContext.Provider value={firebase}>
+      <StoreProvider>
+        <JssProvider jss={jss} generateClassName={generateClassName}>
+          <MuiThemeProvider theme={theme}>
+            <App />
+          </MuiThemeProvider>
+        </JssProvider>
+      </StoreProvider>
+    </FirebaseContext.Provider>
   );
 };
 
